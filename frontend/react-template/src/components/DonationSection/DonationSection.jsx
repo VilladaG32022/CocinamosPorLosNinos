d50121aabc88cd61f4ab43ec2871ade7c297e946
Agsutindt <101265354+VilladaG32022@users.noreply.github.com>
2022-09-15 11:17:30 -0300

    test-

--- frontend/react-template/src/components/DonationSection/DonationSection.jsx
+++ frontend/react-template/src/components/DonationSection/DonationSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import '../DonationSection/DonationSection.css'
 import Button from 'react-bootstrap/Button';
+
+import {links} from '../../utils/constants'
 export default function HomeSection (){
     return(
         <>
@@ -14,7 +16,7 @@ export default function HomeSection (){
                             Tu donación por más chica que sea nos brindará primero apoyo de saber que estamos por un buen camino y luego seguramente un niño más podrá alimentarse. 
                             Las donaciones no son obligatorias ni asumen ningún compromiso futuro, seguro que siempre estaremos esperando una próxima vez, para revalidar de parte tuya que estás de acuerdo en este camino que emprendemos de ahora en más juntos. 
                             </p>
-                            <Button className="donation__btn" href="/donation" variant="primary">DONA!</Button>
+                            <Button className="donation__btn" href={links.DONATE} variant="primary">DONA!</Button>
                         </div>
                     </div>
                 <div className="separator"></div>
