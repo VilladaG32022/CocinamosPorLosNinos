3d9d4bb6b39fd1e56a9e7ab8f2df45a39f251632
Agsutindt <agustindt95@gmail.com>
2022-09-22 12:11:18 -0300

    Route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Donation from "../pages/donate/index";
 import Home from "../pages/home/index";
 
@@ -28,33 +27,4 @@ const routesClient = [
 const routes = [...routesClient];
 
 export default routes;
-=======
-import Donation from '../pages/donate/donation';
-import Home from '../pages/home/home';
 
-import BasicLayout from '../layouts/basiclayout';
-import Layout from '../layouts/layout';
-
-import { links } from '../utils/constants';
-
-const routesClient = [
-  {
-    component: Home,
-    layout: BasicLayout,
-    path: '/',
-  },
-  {
-    component: Home,
-    layout: BasicLayout,
-    path: links.HOME,
-  },
-  {
-    component: Donation,
-    layout: Layout,
-    path: links.DONATE,
-  },
-];
-const routes = [...routesClient];
-
-export default routes;
->>>>>>> ac98cc3fafbb6a5b3b888580fd4602208bd5306d
