2b3fe51d1f146b1e916bb65c33aa24a1c07e37ed
ezemontich <proyectosolidariovillada@gmail.com>
2022-09-28 13:04:12 -0300

    Pricing

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import Card from '../../components/Cards/PricingCard/Card';
+import FirstCard from '../../components/Cards/PricingCard/FirstCard';
 import '../PricingSection/PricingSection.css';
 
 export default function PricingContainer(props) {
   return (
-    <MDBContainer>
-      <MDBRow className="donate__row">
-          <Card btntext="Ir a MercadoPago" />
-      </MDBRow>
-    </MDBContainer>
+    <div>
+      <MDBContainer className='pricing__container'>
+        <MDBRow>
+            <FirstCard btntext="Contacto" />
+        </MDBRow>
+      </MDBContainer>
+      <MDBContainer className='pricing__container'>
+        <MDBRow>
+          <MDBCol>
+            <Card btntext="$50" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$100" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$200" />
+          </MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol>
+            <Card btntext="$500" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$1000" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$2000" />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 }
