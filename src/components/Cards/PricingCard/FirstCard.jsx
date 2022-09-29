9a8388b094a2a0bcc9538ab574173b68397e0dda
Agsutindt <agustindt95@gmail.com>
2022-09-29 10:27:08 -0300

    Problems solved and MP integred

--- src/components/Cards/PricingCard/FirstCard.jsx
+++ src/components/Cards/PricingCard/FirstCard.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { MDBCard, MDBCardTitle, MDBCardText, MDBIcon, MDBCardHeader } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardTitle, MDBCardText, MDBCardHeader } from 'mdb-react-ui-kit';
 import Button from '@mui/material/Button';
 
 import { PricingSec } from '../../../utils/constants';
-import { PricingCardSec } from '../../../utils/constants';
-
 
 export default function App(props) {
   const { btntext } = props;
   return (
     <div>
-        {PricingSec.map((label) => (
-        <MDBCard className='donate__card'>
-        <MDBCardHeader className='donate__header'>
+      {PricingSec.map((label) => (
+        <MDBCard className="donate__card">
+          <MDBCardHeader className="donate__header">
             <MDBCardTitle className="donate__title">{label.title}</MDBCardTitle>
-        </MDBCardHeader>
-            <MDBCardText className="donate__text">{label.text}</MDBCardText>
-            <Button className='donate__button'>
-                {btntext}
-            </Button>
+          </MDBCardHeader>
+          <MDBCardText className="donate__text">{label.text}</MDBCardText>
+          <Button className="donate__button">{btntext}</Button>
         </MDBCard>
-        ))}
+      ))}
     </div>
   );
 }
