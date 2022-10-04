12abe1eaaba58610c39b407238562dbdd6ece6ed
ezemontich <proyectosolidariovillada@gmail.com>
2022-10-04 10:34:37 -0300

    Monto de donacion

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -13,24 +13,24 @@ export default function PricingContainer(props) {
       <div className='pricing__card__container'>
         <MDBRow className="donate__row">
           <MDBCol>
-            <Card btntext="$50" mp="https://mpago.la/32DJ4m1" />
-          </MDBCol>
-          <MDBCol>
-            <Card btntext="$100" mp="https://mpago.la/15gc4bA" />
-          </MDBCol>
-          <MDBCol>
             <Card btntext="$200" mp="https://mpago.la/22kLRso" />
           </MDBCol>
-        </MDBRow>
-        <MDBRow className="donate__row">
           <MDBCol>
             <Card btntext="$500" mp="https://mpago.la/2mGy2ra" />
           </MDBCol>
           <MDBCol>
             <Card btntext="$1000" mp="https://mpago.la/15Yqp8p" />
           </MDBCol>
+        </MDBRow>
+        <MDBRow className="donate__row">
           <MDBCol>
             <Card btntext="$2000" mp="https://mpago.la/1C3HLGY" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$5000" mp="https://mpago.la/15Yqp8p" />
+          </MDBCol>
+          <MDBCol>
+            <Card btntext="$10000" mp="https://mpago.la/1C3HLGY" />
           </MDBCol>
         </MDBRow>
       </div>
