7936427373192a63dfb192e52eb356a6d6972a91
ezemontich <proyectosolidariovillada@gmail.com>
2022-10-04 11:41:11 -0300

    Fotos About

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
-import pic01 from '../assets/Img/pic01.png';
-import pic02 from '../assets/Img/pic02.png';
-import pic03 from '../assets/Img/pic03.png';
-import pic04 from '../assets/Img/pic04.png';
+import pic01 from '../assets/Img/food_yellow.png';
+import pic02 from '../assets/Img/food_grey.png';
+import pic03 from '../assets/Img/food_white.png';
+import pic04 from '../assets/Img/food_green.png';
 
 export const links = {
   HOME: '/',
